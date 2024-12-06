3d84e6cfad3365eda982b769992ca645b71541af
KseniaYudina <uksenia359@gmail.com>
2024-12-06 16:06:03 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+       int originalAmount = 100;
+       int replenishment = 1100;
+       int bonus;
+
+        if (replenishment >= 1000) {
+            bonus = replenishment/100;
+        } else {
+            bonus = 0;
         }
+
+        int finalBalance = originalAmount + replenishment + bonus;
+
+        System.out.println(finalBalance);
     }
 }
